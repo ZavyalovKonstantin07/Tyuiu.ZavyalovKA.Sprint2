09a2727163ea5a871d663df49d8446f0df79288c
Константин Завьялов <Tyuiu.ZavyalovKA@outlook.com>
2025-10-02 13:31:06 +0500

    Add project files.

---
+++ Tyuiu.ZavyalovKA.Sprint2.Task0.V15.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.ZavyalovKA.Sprint2.Task0.V15.Lib
+{
+    public class DataService : ISprint2Task0V15
+    {
+        public bool[] GetCompareOperations(int x, int y)
+        {
+            bool[] res = new bool[6];
+            res[0] = x == y;
+            res[1] = x - 2830 != y;
+            res[2] = x / 15 < y;
+            res[3] = x > y;
+            res[4] = x <= y;
+            res[5] = x / 15 >= y;
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.ZavyalovKA.Sprint2.Task0.V15/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Tyuiu.ZavyalovKA.Sprint2.Task0.V15;
+using Tyuiu.ZavyalovKA.Sprint2.Task0.V15.Lib;
+DataService ds = new DataService();
+int x = 3105;
+int y = 275;
+bool[] res = new bool[6];
+res = ds.GetCompareOperations(x, y);
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("X = " + x);
+Console.WriteLine("Y = " + y);
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+for (int i = 0; i < 6; i++)
+{
+    Console.WriteLine(res[i]);
+}
+Console.ReadKey();
